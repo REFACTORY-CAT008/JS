bdb1b41317cd63878352c1f8808184243060d270
Mark Lary <laryalec12@gmail.com>
2021-06-01 19:33:57 +0300

    modify function name

--- netpay.js
+++ netpay.js
@@ -1,27 +1,27 @@
 
-function bonuses(a) {
+function calculate_bonuses(a) {
     bonus = (a/100) * 1000000;
     console.log(bonus)
     return bonus
 }
 
-function grossPay (slry){
+function calculate_grossPay (slry){
     gPay = slry + bonuses(85);
     console.log(gPay)
     return gPay 
 }
 
-function incomeTax(NSSF, PAYE) {
+function calculate_incomeTax(NSSF, PAYE) {
     taxes = (NSSF + PAYE) * grossPay(3000000);
     console.log(taxes)
      return taxes
 }
 
 
-function netSalary () {
+function calculate_netSalary () {
     netPay = grossPay(3000000) - incomeTax((11/100), (30/100));
     return netPay
 }
 
-console.log(netSalary())
+console.log(calculate_netSalary())
 
