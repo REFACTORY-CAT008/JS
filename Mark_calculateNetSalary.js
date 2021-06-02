55ddfa849140cfa146ba0b3c58d47b71063351eb
Mark Lary <laryalec12@gmail.com>
2021-06-02 10:58:16 +0300

    added comments

--- Mark_calculateNetSalary.js
+++ Mark_calculateNetSalary.js
@@ -1,27 +1,34 @@
-
-function calculate_bonuses(a) {
-    bonus = (a/100) * 1000000;
-    console.log(bonus)
+//declare the function 'calculate_bonuses' and pass it a parameter 'x'
+//parameter 'x' will be the performance
+function calculate_bonuses(x) {
+    // the product of the perfomance percentage and the value 1000000 will result into bonus 
+    bonus = (x/100) * 1000000;
+    //by evoking using return, the value of bonus will be accessed when called in another function
     return bonus
 }
-
-function calculate_grossPay (slry){
-    gPay = slry + bonuses(85);
+//declare the function 'calculate_grosspay' and pass it a parameter 'salary'
+function calculate_grossPay (salary){
+    //the value of gpay is obtained from the sum of salary and function calculate_bonuses
+    gPay = salary + calculate_bonuses(85);
+    //console.log will print the value of gross pay
     console.log(gPay)
+     //by evoking using return, the value of gpay will be accessed when called in another function
     return gPay 
 }
-
+//declare the function 'calculate_incomeTax' and pass it a parameters 'NSSF' and 'PAYE'
 function calculate_incomeTax(NSSF, PAYE) {
-    taxes = (NSSF + PAYE) * grossPay(3000000);
-    console.log(taxes)
+    //the value of taxes is obtained from the product of the sum of the parameters 'NSSF' and 'PAYE' and function calculate_grossPay
+    taxes = (NSSF + PAYE) * calculate_grossPay(3000000);
+    //by evoking using return, the value of taxes will be accessed when called in another function
      return taxes
 }
 
-
+//declare the function 'calculate_netSalary'
 function calculate_netSalary () {
-    netPay = grossPay(3000000) - incomeTax((11/100), (30/100));
+    //to obtain netPay subtract function calculate_incomeTax from function calculate_grossPay
+    netPay = calculate_grossPay(3000000) - calculate_incomeTax((11/100), (30/100));
     return netPay
 }
-
+//console.log will print the value of net salary
 console.log(calculate_netSalary())
 
--- Mark_interprete.js
+++ Mark_interprete.js
@@ -1,4 +1,4 @@
-// //declare the function with variable 'tests' and pass it a parameters 'test1 and test2'
+// //declare the function 'tests' and pass it a parameters 'test1 and test2'
 function tests(test1, test2) {
     //the statement means if test2 is greater than test1 return greater, otherwise return else
     let greater = test2 > test1 ? test1 : test2
@@ -8,35 +8,36 @@ function tests(test1, test2) {
 
 
 //this function would return an error because function is not closed, but i have corrected it
-//declare the function with variable 'coursework' and pass it a parameter 'cswork'
+//declare the function 'coursework' and pass it a parameter 'cswork'
 function coursework(cswork) {
-    //given an argument for parameter cswork, it will give the sum of cswork and result from function with variable name "tests"
+    //given an argument for parameter cswork, it will give the sum of cswork and result from function "tests"
     let coursework = cswork + tests(80, 80)
+    //console.log will print the value of coursework
     console.log(coursework)
     //by invoking using keyword return, the value of the variable will be accessed when called
     return coursework
 
 }
 
-//declare the function with variable 'avg' and pass it a parameter 'a'
+//declare the function 'avg' and pass it a parameter 'a'
 function avg(a) {
-    //the value of variable avg will be obtained by dividing the result of function with variable coursework by parameter a
+    //the value of variable avg will be obtained by dividing the result of function coursework by parameter a
     let avg = coursework(90) / a
 
     return avg
     //by invoking using keyword return, the value of the variable will be accessed when called
 }
 
-//declare the function with variable 'crsmark' and pass it a parameter 'a'
+//declare the function 'crsmark' and pass it a parameter 'a'
 function crsmark(a) {
-    //the product of variable avg and the percentage mark will give the value of exm
+    //the product of function avg and the percentage mark will give the value of exm
     let exm = avg(2) * (a / 100)
     return exm
     //by invoking using keyword return, the value of the variable 'crsmark' will be accessed when called
 }
 
 
-//declare the function with variable 'exam' and pass it a parameter 'a'
+//declare the function 'exam' and pass it a parameter 'a'
 function exam(a) {
     //the value of of fexam will be obtained from the product of the percentage and parameter 'a'
     let fexam = (60 / 100) * a
@@ -46,10 +47,11 @@ function exam(a) {
     //by invoking using keyword return, the value of the variable will be accessed when called
 }
 
-//declare the function with variable 'fnal'
+//declare the function 'fnal'
 function fnal() {
-    //the sum of the results from functions with variable exam and variable crsmark will give the result
+    //the sum of the results from functions exam and crsmark will give the result
     let fmark = exam(75) + crsmark(40)
+       //console.log will print the value of fmark
     console.log(fmark)
 
 }
